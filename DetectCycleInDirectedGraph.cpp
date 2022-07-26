5caef6d69c9e1ba1575c54411cf0d6cc81e5d0d6
sangwan14 <68826136+sangwan14@users.noreply.github.com>
2022-07-26 12:25:53 +0530

    Update DetectCycleInDirectedGraph.cpp

--- DetectCycleInDirectedGraph.cpp
+++ DetectCycleInDirectedGraph.cpp
@@ -60,3 +60,38 @@ class Solution {
         return false;
     }
 };
+
+class Solution3 {
+public:
+    bool isCyclic(int V, vector<int> adj[]) {
+      queue<int> q; 
+	    vector<int> indegree(V, 0); 
+      
+	    for(int i = 0;i<V;i++) {
+	        for(auto it: adj[i]) {
+	            indegree[it]++; 
+	        }
+	    }
+	    
+	    for(int i = 0;i<V;i++) {
+	        if(indegree[i] == 0) {
+	            q.push(i); 
+	        }
+	    }
+	    
+      int cnt = 0;
+	    while(!q.empty()){ 
+	        int node = q.front(); 
+	        q.pop(); 
+	        cnt++;
+	        for(auto it : adj[node]) {
+	            indegree[it]--;
+	            if(indegree[it] == 0) {
+	                q.push(it); 
+	            }
+	        }
+	    }
+      
+      return cnt != V;
+    }
+};
