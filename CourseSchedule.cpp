9c49410182ff7f4d2e8ee8f9e3014ff08204728c
sangwan14 <68826136+sangwan14@users.noreply.github.com>
2022-07-26 12:24:09 +0530

    Update CourseSchedule.cpp

--- CourseSchedule.cpp
+++ CourseSchedule.cpp
@@ -87,34 +87,33 @@ bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
             adj[u].push_back(v);
         }
         
-        vector<int> vis(numCourses,0);
+	queue<int> q; 
+        vector<int> indegree(numCourses, 0); 
+	
+	for(int i = 0;i<numCourses;i++) {
+		for(auto it: adj[i]) {
+		    indegree[it]++; 
+		}
+	}
 
-        queue<int> q; 
-	    vector<int> indegree(numCourses, 0); 
-	    for(int i = 0;i<numCourses;i++) {
-	        for(auto it: adj[i]) {
-	            indegree[it]++; 
-	        }
-	    }
-	    
-	    for(int i = 0;i<numCourses;i++) {
-	        if(indegree[i] == 0) {
-	            q.push(i); 
-	        }
-	    }
+	for(int i = 0;i<numCourses;i++) {
+		if(indegree[i] == 0) {
+		    q.push(i); 
+		}
+	}
 	    
         int cnt = 0;
-	    while(!q.empty()) {
-	        int node = q.front(); 
-	        q.pop(); 
-	        cnt++;
-	        for(auto it : adj[node]) {
-	            indegree[it]--;
-	            if(indegree[it] == 0) {
-	                q.push(it); 
-	            }
-	        }
-	    }
+	while(!q.empty()) {
+		int node = q.front(); 
+		q.pop(); 
+		cnt++;
+		for(auto it : adj[node]) {
+		    indegree[it]--;
+		    if(indegree[it] == 0) {
+			q.push(it); 
+		    }
+		}
+	}
         
         return cnt == numCourses;
     }
